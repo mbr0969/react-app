48770881a14ed692fff98395b858c399a5ddd080
mbr0969 <mbr0969@gmail.com>
2019-05-25 22:04:58 +0300

    25.05.2019 2 video 23

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -2,37 +2,34 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import {NavLink} from 'react-router-dom';
 
-const Dialogs = (props) => {
+const DialogItem = (props) =>{
     return (
+        <div className={s.dialog + '  ' + s.active }>
+            <NavLink  to={"/dialogs/"+props.id}>{props.name}</NavLink>
+        </div>
+    );
+};
+
+const  Dialogs = (props) =>{
+    return(
         <div className={s.dialogs}>
-            <div className={s.dialogsItems}>
-                <NavLink to="#">
-                    <div className={s.dialog + '  ' + s.active }>
-                        Маша
-                    </div>
-                </NavLink>
-                <NavLink to="#">
-                    <div className={s.dialog}>
-                        Мама
-                    </div>
-                </NavLink>
-                <NavLink to="#">
-                    <div className={s.dialog}>
-                        Сеня
-                    </div>
-                </NavLink>
-                <NavLink to="#">
-                    <div className={s.dialog}>
-                        Папа
-                    </div>
-                </NavLink>
-            </div>
+           <div className={s.dialogsItems}>
+               <DialogItem name ='Маша' id="1" />
+               <div className={s.dialog}>
+                   <DialogItem name ='Мама ' id="2" />
+               </div>
+               <div className={s.dialog}>
+                   <DialogItem name ='Сеня' id="3" />
+               </div>
+               <div className={s.dialog}>
+                   <DialogItem name ='Папа' id="4" />
+               </div>
+           </div>
             <div className={s.messages}>
                 <div className={s.message}>Привет</div>
                 <div className={s.message}>Пока</div>
                 <div className={s.message}>Хорошо</div>
             </div>
-
         </div>
     );
 };
