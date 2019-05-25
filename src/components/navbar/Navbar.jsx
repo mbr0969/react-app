c57c85a19004d3efd6a4000c070e56791e0f1137
mbr0969 <mbr0969@gmail.com>
2019-05-25 13:27:15 +0300

    25.05.2019

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import s from './Navbar.module.css';
-
-const Navbar  = () =>{
-
-  return(
-    <nav className={s.nav}>
-    <div className={s.item}><a href="/profile">Профиль</a></div>
-    <div className={`${s.item} ${s.active}`}><a href="/dialogs">Сообщенния</a></div>
-    <div className={s.item}><a href="/news">Новости</a></div>
-    <div className={s.item}><a href="/music">Музыка</a></div>
-    <div className={s.item}><a href="/settings">Настройки</a></div>
-  </nav>
-  );
+import {NavLink} from 'react-router-dom';
+const Navbar = () => {
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}><NavLink to="/profile">Профиль</NavLink></div>
+            <div className={`${s.item} ${s.active}`}><NavLink to="/dialogs">Сообщенния</NavLink></div>
+            <div className={s.item}><NavLink to="/news">Новости</NavLink></div>
+            <div className={s.item}><NavLink to="/music">Музыка</NavLink></div>
+            <div className={s.item}><NavLink to="/settings">Настройки</NavLink></div>
+        </nav>
+    );
 };
 
 export default Navbar;
